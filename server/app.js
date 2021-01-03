88b3424dd3a64eea82feeccb317bc51691bdd1ef
ehbong <hangillw86@gmail.com>
2021-01-03 17:09:38 +0900

    테스트 코드 작성

--- server/app.js
+++ server/app.js
@@ -8,6 +8,8 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var mainRouter = require("./routes/main");
+var mainApiRouter = require("./routes/api/main");
 
 var app = express();
 
@@ -25,6 +27,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/main", mainRouter);
+app.use("/api/main", mainApiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ server/routes/api/main.js
@@ -0,0 +1,14 @@
+/** @format */
+
+const { json } = require("express");
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("", function (req, res, next) {
+  return res.status(200).json({
+    success: true,
+  });
+});
+
+module.exports = router;
